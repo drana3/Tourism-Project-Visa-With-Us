19fcf296563dd19e1c8eb2dfe18da65e9c7465d1
drana3 <31128429+drana3@users.noreply.github.com>
2025-11-22 13:05:24 +0530

    Update prep.py

--- tourism_project/model_building/prep.py
+++ tourism_project/model_building/prep.py
@@ -12,7 +12,7 @@ from huggingface_hub import login, HfApi
 
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
-DATASET_PATH = "hf://datasets/Dewasheesh/Tourism-Project-Visa-With_Us/tourism.csv"
+DATASET_PATH = "hf://datasets/Dewasheesh/Tourism-Project-Visa-With-Us/tourism.csv"
 df = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
